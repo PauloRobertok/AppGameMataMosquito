0874ccf1ca5d5078dac1718ef7d77c9ae732fe92
devPaulo <paulora999@gmail.com>
2022-06-20 22:52:21 -0300

    Tamanho e disposição dos mosquitos, surgimento dos mosquitos e criação do fundo de jogo.

--- jogo.js
+++ jogo.js
@@ -5,29 +5,59 @@ function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
 
-    console.log(largura,altura)
+    console.log(largura, altura)
 }
 
 ajustaTamanhoPalcoJogo()
 
 function posicaoRandomica() {
 
-    var posicaoX = Math.floor ( Math.random() * largura ) -90
-    var posicaoY = Math.floor ( Math.random() * altura ) -90
+    //Remover Mosquito anterior (caso exista)
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
 
     posicaoX = posicaoX < 0 ? 0 : posicaoX
     posicaoY = posicaoY < 0 ? 0 : posicaoY
 
     console.log(posicaoX, posicaoY)
-//Criar elemento HMTL
+    //Criar elemento HMTL
 
-var mosquito = document.createElement('img')
-mosquito.src = 'imagens/mosquito.png'
-mosquito.className = "mosquito1"
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
 
-document.body.appendChild(mosquito)
+    document.body.appendChild(mosquito)
 
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
